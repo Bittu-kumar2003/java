3ec86385791fe1ca50dee12d973971cf7d34e2de
Bittu-kumar2003 <109884531+Bittu-kumar2003@users.noreply.github.com>
2022-10-08 19:20:27 +0530

    Add files via upload

---
+++ evennumber.java
@@ -0,0 +1,10 @@
+public class evennumber {
+	public static void main(String[]args) {
+		int i ;
+		for(i=1;i<5;i++) 
+		//System.out.println(i);
+		//System.out.println(2*i+1);
+            System.out.println(2*i);
+	}
+}
+	
